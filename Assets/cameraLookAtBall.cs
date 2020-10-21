679207322957cc2cd5e0a59e3031335679bca551
ankur786-coder <63516933+ankur786-coder@users.noreply.github.com>
2020-10-21 18:17:25 +0530

    Added All Files

---
+++ Assets/cameraLookAtBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraLookAtBall : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 lookAtOffSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAtOffSet = new Vector3(0, 1.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ball.transform.position + lookAtOffSet);
+    }
+}
